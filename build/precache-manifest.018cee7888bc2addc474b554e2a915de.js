fa2240e00e134464b5c3e1bf9d411879092303f9
Dendolium <dendodendolium@gmail.com>
2020-03-27 10:09:55 +0500

    build forlder added

---
+++ build/precache-manifest.018cee7888bc2addc474b554e2a915de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b2d14ab493f8f4ac374241b3cf68c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d8b912645907566245a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b9dad17f57d8c01599ca",
+    "url": "/static/css/main.ce6a8de3.chunk.css"
+  },
+  {
+    "revision": "5d8b912645907566245a",
+    "url": "/static/js/2.47fa365a.chunk.js"
+  },
+  {
+    "revision": "135fff72a12912cebdb2ab1f22f8bab1",
+    "url": "/static/js/2.47fa365a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9dad17f57d8c01599ca",
+    "url": "/static/js/main.a96df4f6.chunk.js"
+  },
+  {
+    "revision": "3db821147e137c32490f",
+    "url": "/static/js/runtime-main.67998282.js"
+  }
+]);
